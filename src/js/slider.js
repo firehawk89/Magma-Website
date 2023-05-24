0fcd6606dfa46289e3c9ebff1b58cbaec71b41d4
Bochkovskyi <unundertale1@ukr.net>
2023-05-24 20:09:44 +0300

    add short blog section

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,24 +1,48 @@
-const reviewsSlider = new Swiper(".reviews__slider", {
-  slidesPerView: 1.3,
-  spaceBetween: 23,
+/* REVIEWS SLIDER */
+if (document.querySelector(".reviews__slider") != null) {
+  new Swiper(".reviews__slider", {
+    slidesPerView: 1.3,
+    spaceBetween: 23,
 
-  navigation: {
-    enabled: false,
-  },
+    navigation: {
+      enabled: false,
+    },
 
-  breakpoints: {
-    992: {
-      slidesPerView: 1,
+    breakpoints: {
+      992: {
+        slidesPerView: 1,
 
-      navigation: {
-        enabled: true,
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        navigation: {
+          enabled: true,
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+
+      769: {
+        slidesPerView: 1.6,
       },
     },
+  });
+}
 
-    769: {
-      slidesPerView: 1.6,
-    },
-  },
-});
+/* BLOG SLIDER */
+const homeBlogItemsContainer = document.querySelector(".short-blog__items");
+/* const homeBlogItemsBody = document.querySelector(".home-blog__items-body");
+const blogItems = document.querySelectorAll(".blog__item"); */
+
+if (homeBlogItemsContainer != null) {
+  if (window.matchMedia("(max-width: 767.98px)").matches) {
+    /* slider initialization */
+    /*     homeBlogItemsContainer.classList.add("swiper");
+    homeBlogItemsBody.classList.add("swiper-wrapper");
+    blogItems.forEach((item) => {
+      item.classList.add("swiper-slide");
+    }); */
+
+    new Swiper(".short-blog__items", {
+      slidesPerView: 1.1,
+      spaceBetween: 23,
+    });
+  }
+}
