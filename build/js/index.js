bac5e474ae1085bde596831548325b8901a6e344
Bochkovskyi <unundertale1@ukr.net>
2023-05-30 12:55:54 +0300

    add preloader, fix video, refactor

--- build/js/index.js
+++ build/js/index.js
@@ -1,3 +1,34 @@
+/* PRELOADER */
+const preloader = document.querySelector(".preloader");
+document.documentElement.classList.add("is-locked");
+document.body.classList.add("is-locked");
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.documentElement.classList.remove("is-locked");
+  document.body.classList.remove("is-locked");
+  fadeOut(preloader, 200);
+  //$("#preloader").fadeOut(200);
+});
+
+/* PRELOADER FADE OUT ANIMATION */
+function fadeOut(element, duration) {
+  var opacity = 1;
+  var interval = 10; // interval in milliseconds, transition should be same length
+  var steps = Math.ceil(duration / interval);
+  var delta = 1 / steps;
+
+  var timer = setInterval(function () {
+    opacity -= delta;
+    element.style.opacity = opacity;
+
+    if (opacity <= 0) {
+      clearInterval(timer);
+      element.style.opacity = 0;
+      element.style.display = "none";
+    }
+  }, interval);
+}
+
 /* SCROLL PADDING FOR FIXED HEADER */
 const headerHeight = document.querySelector(".header").offsetHeight;
 document.documentElement.style.setProperty(
